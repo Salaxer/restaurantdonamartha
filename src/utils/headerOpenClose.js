cfdb291f38cc8f16f41cd63e7601ccfdbd465a52
Salaxer <hector_salazar12@hotmail.com>
2021-09-23 08:55:26 -0500

    version 1.0.0 of the header ending

--- src/utils/headerOpenClose.js
+++ src/utils/headerOpenClose.js
@@ -1,13 +1,14 @@
 const headerOpenClose = () =>{
     let menu = document.getElementById('header_nav');    
     let checkBox = document.getElementById('check');
+    let max_width = window.innerWidth;
     checkBox.addEventListener('change', ()=>{
-        let max_width = window.innerWidth;
-        if (checkBox.checked) {
-            menu.style.left = '0px';
-        }else{
-            menu.style.left = `-${max_width*0.6}px`
-            console.log(`-${max_width*0.6}px`);
+        checkBox.checked ? menu.style.left = '0px' : menu.style.left = `-${max_width*0.6}px`;
+    })
+    window.addEventListener('click',(ev)=>{
+        if (ev.target !== menu && ev.target !== checkBox) {
+            menu.style.left = `-${max_width*0.6}px`;
+            checkBox.checked = false;
         }
     })
 }
