cc6cdbedfae20de5a30d80b10c2cadc63054b1bb
Salaxer <hector_salazar12@hotmail.com>
2021-10-15 10:56:53 -0500

    Hola

--- src/components/Fb_opinios.jsx
+++ src/components/Fb_opinios.jsx
@@ -5,8 +5,7 @@ const Fb_opinions = () => {
   return (
       <>
         <div className="main__opinions">
-            <div className="fb-page" data-href="https://web.facebook.com/Micheladas-Do%C3%B1a-Martha-101296578313615" data-tabs="timeline" data-width="500" data-height="70" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://web.facebook.com/Micheladas-Do%C3%B1a-Martha-101296578313615" className="fb-xfbml-parse-ignore"><a href="https://web.facebook.com/Micheladas-Do%C3%B1a-Martha-101296578313615">Micheladas Doña Martha</a></blockquote></div>
-            <div data-colorscheme="dark" className="fb-comments" data-href="https://web.facebook.com/Micheladas-Do%C3%B1a-Martha-101296578313615/?ref=page_internal" data-numposts="1" data-width="100%"></div>
+        <div style={{display: "block"}} className="fb-page" data-href="https://www.facebook.com/Micheladas-Do&#xf1;a-Martha-101296578313615/" data-tabs="timeline,events,messages" data-width="500" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Micheladas-Do&#xf1;a-Martha-101296578313615/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Micheladas-Do&#xf1;a-Martha-101296578313615/">Micheladas Doña Martha</a></blockquote></div>
         </div>
       </>
   );
