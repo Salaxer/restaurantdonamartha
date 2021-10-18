24ee87eef7364ee18b281e4d1f1dba8f28810e8d
Salaxer <hector_salazar12@hotmail.com>
2021-10-18 14:33:46 -0500

    little menu almost ending

--- src/components/MenuFood.jsx
+++ src/components/MenuFood.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import '../'
+import { Link } from 'react-router-dom';
+import '../assets/styles/menuFood.css';
 
 const MenuFood = () => {
   return (
       <>
         <div className="grid_Menu">
-         Hello
+          <h1 className="general_title_little_menu">Lo mas destacado en el restaurante</h1>
+          <div className="target_food">
+              <h1>Mas pedido</h1>
+              <p>Mojarra frita</p>
+              <Link to="./hi" className="container_image_little">
+                <img src="https://antojandoando.com/wp-content/uploads/2020/04/face.jpg" alt="mojarra frita" srcset="" />
+              </Link>
+          </div>
+          <div className="target_food">
+              <h1>Mejor valorado</h1>
+              <p>Mojarra frita</p>
+              <Link to="./hi" className="container_image_little">
+                <img src="https://antojandoando.com/wp-content/uploads/2020/04/face.jpg" alt="mojarra frita" srcset="" />
+              </Link>
+          </div>
+          <div className="target_food">
+              <h1>Recomendado</h1>
+              <p>Mojarra frita</p>
+              <Link to="./hi" className="container_image_little">
+                <img src="https://antojandoando.com/wp-content/uploads/2020/04/face.jpg" alt="mojarra frita" srcset="" />
+              </Link>
+          </div>
+          <div className="target_food">
+              <h1>Nose</h1>
+              <p>Mojarra frita</p>
+              <Link to="./hi" className="container_image_little">
+                <img src="https://antojandoando.com/wp-content/uploads/2020/04/face.jpg" alt="mojarra frita" srcset="" />
+              </Link>
+          </div>
+          <div className="link_all_menu target_food">
+              <Link to="./hi">
+                <h1>View all Menu</h1>
+              </Link>
+          </div>
         </div>
       </>
   );
