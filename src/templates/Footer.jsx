f6fcb07f3534c3d03b98d34865534fefbe76f76b
Salaxer <hector_salazar12@hotmail.com>
2021-10-20 09:38:45 -0500

    Se arregaron bugs en el footer el cual no peermitia un buen responsive

--- src/templates/Footer.jsx
+++ src/templates/Footer.jsx
@@ -5,9 +5,30 @@ import '../assets/styles/footer.css';
 const Footer = () => {
   return (
     <div className="Footer">
-      <p className="Footer-title"> Bay</p>
-      <p className="Footer-copy"> Contacto </p>
-      {/* <Find_us></Find_us> */}
+      <div className="contact_us">
+        <div className="tel">Contactanos al <a href="tel:+52777g5073272"> 7775073272</a></div>
+        <div>
+          O bien envianos un mensaje por whatsapp:
+        </div>
+        <div className="social">
+          Siguenos en nuestras redes sociales:
+          <br />
+          <a href="https://www.facebook.com/Micheladas-Do%C3%B1a-Martha-101296578313615/" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook"></i></a>
+          <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fab fa-twitter"></i></a>
+          <a href="http://" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+        </div>
+      </div>
+      <div className="hours">
+        Business Hours
+        Mon:	10:05 AM – 8:00 PM
+        Tue:	10:05 AM – 8:10 PM
+        Wed:	10:05 AM – 8:10 PM
+        Thu:	10:05 AM – 8:10 PM
+        Fri:	10:05 AM – 8:10 PM
+        Sat:	10:05 AM – 8:10 PM
+        Sun:	10:05 AM – 8:10 PM
+      </div>
+      <Find_us></Find_us>
     </div>
   );
 };
