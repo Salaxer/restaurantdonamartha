d0f283532ca8cd9beb18bce596e9785a00c22393
Salaxer <hector_salazar12@hotmail.com>
2021-11-09 15:08:14 -0600

    auth realize

---
+++ src/auth.js
@@ -0,0 +1,48 @@
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword   } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export const Google = () =>{
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+}
+
+export const Facebook = () =>{
+    return console.log('Hello');
+}
+
+export const Email = ({name, email, password}) =>{
+    console.log(email);
+    console.log(password);
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+
+export default {};
--- src/pages/sign_in.jsx
+++ src/pages/sign_in.jsx
@@ -1,51 +1,46 @@
 import React, { useState } from 'react';
 
-
+import imgGoogle from '../assets/5847f9cbcef1014c0b5e48c8.png'
 import '../assets/styles/sign_in.css';
 
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import passwordValidator from 'password-validator'; 
 
-const provider = new GoogleAuthProvider();
+import {Google, Facebook, Email} from '../auth';
 
-const auth = getAuth();
-
+var schema = new passwordValidator();
+schema
+.is().min(8)                                    // Minimum length 8
+.is().max(100)                                  // Maximum length 100
+.has().uppercase()                              // Must have uppercase letters
+.has().lowercase()                              // Must have lowercase letters
+.has().digits(2)                                // Must have at least 2 digits
 
 const Signin = () => {
   
-  const Google = () =>{
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      console.log(user);
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-  }
+  
   const [form, setForm] = useState({data:{name: "", email: "",password: ""}});
 
   const formValue = (e) =>{
+    const buttonSend = document.getElementById('buttonSend');
     setForm({
       data: { 
           ...form.data,
           [e.target.name]: e.target.value
       }
     });
+    const spanError = document.getElementById('errorFom');
+    const errorPassword = e.target.name == 'password' ? schema.validate(e.target.value, {details: true}): [];
+    if (errorPassword.length == 0) {
+      buttonSend.disabled = false;
+      spanError.style.visibility = 'hidden';
+    }else{
+      buttonSend.disabled = true;
+      spanError.style.visibility = 'visible';
+      spanError.innerText = `${errorPassword.map(item => {return item.message})}`
+    }
     const formEmail = document.getElementById('textForEmail');
     if (e.target.name == 'email' && e.target.value != "") {
       formEmail.classList.add('validemail')
-      console.log(e.target.value);
     }else if(e.target.name == 'email' && e.target.value == ""){
       formEmail.classList.remove('validemail')
     }
@@ -56,8 +51,8 @@ const Signin = () => {
     <div className="Sign_inView">
       <div className="Sign_in">
         <h1 className="title">Registro</h1>
-        <button className="inputs buttons Google" onClick={Google}><img className="Google-img" src="http://assets.stickpng.com/images/5847f9cbcef1014c0b5e48c8.png" alt="google image"/> Registrate con Google </button>
-        <button className="inputs buttons Facebook"> <i className="fab fa-facebook"></i> Registrate con Facebook </button>
+        <button className="inputs buttons Google" onClick={Google}><img className="Google-img" src={imgGoogle} alt="google image"/> Registrate con Google </button>
+        <button className="inputs buttons Facebook" onClick={Facebook}> <i className="fab fa-facebook"></i> Registrate con Facebook </button>
         <div className="divisorLine">
           <span className="divisorLine-text">O</span>
         </div>
@@ -67,7 +62,8 @@ const Signin = () => {
         <label className="textForEmail" id="textForEmail" htmlFor="email">Correo electronico</label>
         <input className="inputs inputsText" id="password" name="password" type="password" autoComplete="true" required onChange={(e) => formValue(e)}/>
         <label className="textForPass" htmlFor="password">Contrase&ntilde;a</label>
-        <button className="inputs buttons buttonRegister">Registrarse</button>
+        <button className="inputs buttons buttonRegister" id="buttonSend" disabled onClick={Email({...form.data})}>Registrarse</button>
+        <span style={{visibility:'hidden'}} id="errorFom" className="someError"></span>
       </div>
     </div>
   )
